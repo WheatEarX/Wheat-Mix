2ba8864bd85cef55bc70886f929ef4595f87551a
WheatEar <wheat_ear@outlook.com>
2024-04-03 05:52:29 +0800

    Calculator V4

--- src/main/java/com/wheat_ear/screen/CalculatorScreen.java
+++ src/main/java/com/wheat_ear/screen/CalculatorScreen.java
@@ -14,18 +14,21 @@ import java.math.RoundingMode;
 
 public class CalculatorScreen extends Screen {
     private static final Text TITLE = Text.translatable("menu.calculator");
+    private static final int BUTTON_WIDTH = 25;
     private static final int[] NUMBER_ORDER = {7, 8, 9, 4, 5, 6, 1, 2, 3, 0};
     private static final ButtonType[] BUTTON_ORDER = {
-            ButtonType.POWER, ButtonType.POWER2, ButtonType.POWER3, ButtonType.BACKSPACE,
-            ButtonType.SQUARE_ROOT, ButtonType.ABS, ButtonType.ALL_CLEAR, ButtonType.DIVIDE,
-            ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.MULTIPLY,
-            ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.SUBTRACT,
-            ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.ADD,
-            ButtonType.NEGATE, ButtonType.NUMBER, ButtonType.POINT, ButtonType.EQUALS
+            ButtonType.PI, ButtonType.E, ButtonType.REVERSE, ButtonType.ABS, ButtonType.MOD,
+            ButtonType.EXP, ButtonType.POWER, ButtonType.POWER2, ButtonType.POWER3, ButtonType.BACKSPACE,
+            ButtonType.SIN, ButtonType.SQUARE_ROOT, ButtonType.CUBE_ROOT, ButtonType.ALL_CLEAR, ButtonType.DIVIDE,
+            ButtonType.COS, ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.MULTIPLY,
+            ButtonType.TAN, ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.SUBTRACT,
+            ButtonType.LOG, ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.NUMBER, ButtonType.ADD,
+            ButtonType.LOG10, ButtonType.NEGATE, ButtonType.NUMBER, ButtonType.POINT, ButtonType.EQUALS
     };
     private static TextFieldWidget textField;
     private static BigDecimal number1, number2;
     private static Operator operator;
+    private static final GridWidget gridWidget = new GridWidget();
     private static InputMode inputMode = InputMode.FIRST;
 
     public CalculatorScreen(MinecraftClient client) {
@@ -34,7 +37,7 @@ public class CalculatorScreen extends Screen {
         this.client = client;
         textRenderer = this.client.textRenderer;
 
-        textField = new TextFieldWidget(textRenderer, 400, 300, 130, 15, Text.empty());
+        textField = new TextFieldWidget(textRenderer, 400, 300, 125, 15, Text.empty());
 
         addDrawableChild(textField);
     }
@@ -44,10 +47,9 @@ public class CalculatorScreen extends Screen {
         textField.active = false;
         textField.visible = true;
 
-        GridWidget gridWidget = new GridWidget();
-        GridWidget.Adder adder = gridWidget.createAdder(4);
+        GridWidget.Adder adder = gridWidget.createAdder(5);
 
-        adder.add(textField, 4);
+        adder.add(textField, 5);
         addNumbers(adder);
 
         gridWidget.refreshPositions();
@@ -61,13 +63,48 @@ public class CalculatorScreen extends Screen {
             if (buttonType == ButtonType.NUMBER) {
                 String number = Integer.toString(NUMBER_ORDER[numberIndex]);
                 adder.add(ButtonWidget.builder(Text.of(number),
-                        button -> textField.write(number)).width(30).build());
+                        button -> textField.write(number)).width(BUTTON_WIDTH).build());
                 ++numberIndex;
             }
             else {
                 adder.add(buttonType.getButton());
             }
         }
+    }
+
+    private static void pressButton(ButtonType type) {
+        type.getButton().onPress();
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        // client.player.sendMessage(Text.literal(Integer.toString(keyCode)));
+        super.keyPressed(keyCode, scanCode, modifiers);
+        if (keyCode >= 48 && keyCode <= 57) {   // Numbers
+            String number = Integer.toString(keyCode - 48);
+            ButtonWidget.builder(Text.of(number),
+                    button -> textField.write(number)).build().onPress();
+        }
+        switch (keyCode) {
+            case 61 -> {   // + and =
+                if (Screen.hasShiftDown()) {
+                    pressButton(ButtonType.ADD);
+                }
+                else {
+                    pressButton(ButtonType.EQUALS);
+                }
+            }
+            case 45 -> pressButton(ButtonType.SUBTRACT);   // -
+            case 56 -> {    // *
+                if (Screen.hasShiftDown()) {
+                    pressButton(ButtonType.MULTIPLY);
+                }
+            }
+            case 47 -> pressButton(ButtonType.DIVIDE);   // /
+            case 259 -> pressButton(ButtonType.BACKSPACE);  // <-
+            case 67 -> pressButton(ButtonType.ALL_CLEAR);   // AC
+        }
+        return true;
     }
 
     private static void setNumber() {
@@ -107,6 +144,40 @@ public class CalculatorScreen extends Screen {
     private static void setNumberAndClear() {
         setNumber();
         textField.setText("");
+        if (number1 != null) {
+            textField.setText(number1.toString());
+        }
+    }
+
+    private static void normalOperate() {
+        try {
+            setNumber();
+            if (number1 != null && number2 != null) {
+                textField.setText(operator.method.get().toString());
+            }
+        }
+        catch (NumberFormatException | ArithmeticException | NullPointerException e) {
+            textField.setText("格式错误");
+        }
+    }
+
+    private static void operate() {
+        try {
+            setNumberAndClear();
+            if (number1 != null && number2 != null) {
+                textField.setText(operator.method.get().toString());
+                number1 = new BigDecimal(textField.getText());
+                inputMode = InputMode.FIRST;
+            }
+        }
+        catch (NumberFormatException | ArithmeticException | NullPointerException e) {
+            textField.setText("格式错误");
+        }
+    }
+
+    private static void allClear() {
+        number1 = number2 = null;
+        syncNumberText();
     }
 
     enum ButtonType {
@@ -116,32 +187,31 @@ public class CalculatorScreen extends Screen {
         SUBTRACT("-", Operator.SUBTRACT),
         MULTIPLY("*", Operator.MULTIPLY),
         DIVIDE("/", Operator.DIVIDE),
-        ALL_CLEAR("C", button -> {
-            number1 = number2 = null;
-            syncNumberText();
-        }),
-        EQUALS("=", button -> {
-            setNumberAndClear();
-            if (number2 != null) {
-                textField.setText(operator.method.get().toString());
-                number1 = new BigDecimal(textField.getText());
-                inputMode = InputMode.FIRST;
-            }
-        }),
+        MOD("%", Operator.MOD),
+        ALL_CLEAR("AC", button -> allClear()),
+        EQUALS("=", button -> operate()),
         POINT(".", button -> textField.write(".")),
-        NEGATE("⁺/₋", Operator.NEGATE),
+        NEGATE("+/-", Operator.NEGATE),
         SQUARE_ROOT("√", Operator.SQUARE_ROOT),
+        CUBE_ROOT("³√", Operator.CUBE_ROOT),
         POWER("xʸ", Operator.POWER),
         POWER2("x²", Operator.POWER, new BigDecimal(2)),
         POWER3("x³", Operator.POWER, new BigDecimal(3)),
         ABS("|x|", Operator.ABS),
-        REVERSE;
+        REVERSE("1/x", Operator.REVERSE),
+        PI("π", new BigDecimal("3.1451926535897")),
+        E("e", new BigDecimal("2.7182818284590")),
+        LOG10("log₁₀", Operator.LOG10),
+        LOG("log", Operator.LOG),
+        SIN("sin", Operator.SIN),
+        COS("cos", Operator.COS),
+        TAN("tan", Operator.TAN),
+        EXP("exp", button -> textField.write("e"));
 
         private String string;
         private ButtonWidget.PressAction action;
 
         ButtonType() {
-
         }
 
         ButtonType(String string, ButtonWidget.PressAction action) {
@@ -153,6 +223,7 @@ public class CalculatorScreen extends Screen {
             this.string = string;
             if (operator.isTwoArgs) {
                 this.action = button -> {
+                    normalOperate();
                     setNumberAndClear();
                     CalculatorScreen.operator = operator;
                     inputMode = InputMode.SECOND;
@@ -169,7 +240,7 @@ public class CalculatorScreen extends Screen {
 
         ButtonType(String string, Operator operator, BigDecimal arg2) {
             this.string = string;
-            if (!operator.isTwoArgs) {
+            if (operator.isTwoArgs) {
                 this.action = button -> {
                     setNumber();
                     number2 = arg2;
@@ -180,8 +251,13 @@ public class CalculatorScreen extends Screen {
             }
         }
 
+        ButtonType(String string, BigDecimal value) {
+            this.string = string;
+            this.action = button -> textField.setText(value.toString());
+        }
+
         public ButtonWidget getButton() {
-            return ButtonWidget.builder(Text.of(string), action).width(30).build();
+            return ButtonWidget.builder(Text.of(string), action).width(BUTTON_WIDTH).build();
         }
     }
 
@@ -191,12 +267,23 @@ public class CalculatorScreen extends Screen {
         MULTIPLY(() -> number1.multiply(number2), true),
         DIVIDE(() -> number1.divide(number2, 7, RoundingMode.HALF_EVEN), true),
         POWER(() -> number1.pow(number2.intValue()), true),
+        MOD(() -> number1.remainder(number2), true),
         NEGATE(() -> number1.negate()),
         SQUARE_ROOT(() -> number1.sqrt(MathContext.DECIMAL64)),
-        ABS(() -> number1.abs());
+        CUBE_ROOT(() -> getValueFromDouble(Math.cbrt(number1.doubleValue()))),
+        ABS(() -> number1.abs()),
+        REVERSE(() -> new BigDecimal(1).divide(number1, 7, RoundingMode.HALF_EVEN)),
+        LOG10(() -> getValueFromDouble(Math.log10(number1.doubleValue()))),
+        LOG(() -> getValueFromDouble(Math.log(number1.doubleValue()))),
+        SIN(() -> getValueFromDouble(Math.sin(number1.doubleValue()))),
+        COS(() -> getValueFromDouble(Math.cos(number1.doubleValue()))),
+        TAN(() -> getValueFromDouble(Math.tan(number1.doubleValue())));
 
         public final OperateAction<BigDecimal> method;
         public final boolean isTwoArgs;
+        static BigDecimal getValueFromDouble(double result) {
+            return BigDecimal.valueOf(result);
+        }
 
         Operator(OperateAction<BigDecimal> method) {
             this.method = method;
@@ -215,6 +302,6 @@ public class CalculatorScreen extends Screen {
     }
 
     interface OperateAction<T> {
-        T get();
+        T get() throws NumberFormatException, ArithmeticException;
     }
 }
